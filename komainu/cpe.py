5d27fc46534de8dfeb2e10488d0142472cd1b127
Mabry Cervin <mpcervin@uncg.edu>
2017-04-03 23:14:28 -0400

    Added functions to get NVD id and config from CPE entries

--- komainu/cpe.py
+++ komainu/cpe.py
@@ -2,9 +2,15 @@ from lxml import etree
 
 from komainu.common import load_xml_file
 
-def get_entries(cpe_file):
+def load_entries_from_file(cpe_file):
     xml = load_xml_file(cpe_file)
     # Points to all config elements of the cpe document
     element_path = './/{http://cpe.mitre.org/dictionary/2.0}cpe-item[@name]'
     root = xml.getroot()
-    return root.iterfind(element_path)
+    return root, root.iterfind(element_path)
+
+def get_config(cpe_entry):
+    return cpe_entry.get('name')
+
+def get_nvdid(cpe_entry):
+    return cpe_entry.find('.//{http://scap.nist.gov/schema/cpe-dictionary-metadata/0.2}item-metadata').get('nvd-id')
