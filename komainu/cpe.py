5df195f4694b678042b69962e436a19cc28bc38a
Mabry Cervin <mpcervin@uncg.edu>
2017-04-04 20:08:19 -0400

    Changed CPE parsing to be namespace aware

--- komainu/cpe.py
+++ komainu/cpe.py
@@ -4,13 +4,19 @@ from komainu.common import load_xml_file
 
 def load_entries_from_file(cpe_file):
     xml = load_xml_file(cpe_file)
-    # Points to all config elements of the cpe document
-    element_path = './/{http://cpe.mitre.org/dictionary/2.0}cpe-item[@name]'
     root = xml.getroot()
+    namespace = root.nsmap[None]
+    # Points to all config elements of the cpe document
+    element_path = './/{{{0}}}cpe-item[@name]'.format(namespace)
     return root, root.iterfind(element_path)
+
 
 def get_config(cpe_entry):
     return cpe_entry.get('name')
 
-def get_nvdid(cpe_entry):
-    return cpe_entry.find('.//{http://scap.nist.gov/schema/cpe-dictionary-metadata/0.2}item-metadata').get('nvd-id')
+
+def make_get_nvdid(root):
+    meta = root.nsmap['meta']
+    def get_nvdid(cpe_entry):
+        return cpe_entry.find('.//{{{0}}}item-metadata'.format(meta)).get('nvd-id')
+    return get_nvdid
